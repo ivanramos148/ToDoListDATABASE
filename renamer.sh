69d170370bd78fb22033eb4527ee6af0cfeeae3a
camander321 <chamburg321@gmail.com>
2018-02-19 21:27:26 -0800

    add bash script for project renaming

---
+++ renamer.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+old="template"
+new="$1"
+
+cp -r . "../$new.Solution"
+cd "../$new.Solution"
+rm renamer.sh
+
+list()
+{
+  for file in *
+  do
+    if [ "$file" != "bin" ] && [ "$file" != "obj" ] && [ "$file" != ".git" ]
+    then
+      if [ -d "$file" ]
+      then
+        cd $file
+        local currentFile=$file
+        list
+        file=$currentFile
+        cd ..
+      else
+        sed -i -e "s/$old/$new/g" "$file"
+      fi
+
+      newname="${file/$old/$new}"
+      oldname="$file"
+      if [ "$oldname" != "$newname" ] && [ -e $file ]
+      then
+        mv "$oldname" "$newname" && echo "$oldname > $newname"
+      fi
+      
+    fi
+  done
+}
+
+list
